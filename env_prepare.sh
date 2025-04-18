0583f22612f0c0a1c66e7b697fdf3c76c01f6da6
Haisheng Chen <hac048@ucsd.edu>
2025-04-17 23:21:57 -0700

    update env_prepare.sh

--- env_prepare.sh
+++ env_prepare.sh
@@ -12,3 +12,12 @@ conda install cmake
 # build libraft
 cd kernels/3rdparty/raft
 ./build.sh libraft
+
+cd kernels
+mkdir build && cd build
+cmake ..
+make -j
+
+# This will automatically build and link the operators
+cd quest/ops
+bash setup.sh
