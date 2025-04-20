0ada4fefb204fddc7f28d97917bc42dedb9f2e8d
Haisheng Chen <hac048@ucsd.edu>
2025-04-19 18:54:06 -0700

    update env_prepare.sh

--- env_prepare.sh
+++ env_prepare.sh
@@ -1,23 +1,2 @@
-
-conda activate quest
-# Quest
-pip install -e .
-# Flash-Attention
-pip install ninja packaging
-pip install flash-attn==2.6.3 --no-build-isolation
-
-# Install CMake (with version >= 3.26.4)
-conda install cmake
-
-# build libraft
-cd kernels/3rdparty/raft
-./build.sh libraft
-
-cd kernels
-mkdir build && cd build
-cmake ..
-make -j
-
-# This will automatically build and link the operators
-cd quest/ops
-bash setup.sh
+cd kernels/3rdparty/raft && bash build.sh libraft
+cd /workspace/quest/quest/ops && bash setup.sh
