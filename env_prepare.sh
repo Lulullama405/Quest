c94643073b57a111b30d5720473fb2c11a4d6fb5
Haisheng Chen <hac048@ucsd.edu>
2025-04-19 20:31:39 -0700

    update env prepare.sh

--- env_prepare.sh
+++ env_prepare.sh
@@ -1,4 +1,9 @@
+source /opt/conda/etc/profile.d/conda.sh
+conda activate quest
+rm -rf /workspace/quest
+git clone --recurse-submodules https://github.com/lulullama405/quest /workspace/quest
 cd kernels/3rdparty/raft && bash build.sh libraft
 cd /workspace/quest/quest/ops && bash setup.sh
+ln -s /mount/quest_data  /workspace/quest/evaluation/LongBench/pred
 
-ln -s /mount/quest_data    /workspace/quest/evaluation/LongBench/pred
+cd /workspace/quest && bash scripts/longbench.sh
