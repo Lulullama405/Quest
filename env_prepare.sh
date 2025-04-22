07832da9ef2fb75053725d5bbad4598602fd452f
Haisheng Chen <hac048@ucsd.edu>
2025-04-21 22:17:15 -0700

    update

--- env_prepare.sh
+++ env_prepare.sh
@@ -3,7 +3,7 @@ conda activate quest
 git pull
 cd kernels/3rdparty/raft && bash build.sh libraft
 cd /workspace/quest/quest/ops && bash setup.sh
-ln -s /mount/quest_data/quest_data  /workspace/quest/evaluation/LongBench/pred
+ln -s /mount/quest_data/data  /workspace/quest/evaluation/LongBench/pred
 git clone https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Llama-8B "/mnt/R1-Distill-llama-8B"
 pip install rouge
 cd /workspace/quest && bash scripts/longbench.sh
