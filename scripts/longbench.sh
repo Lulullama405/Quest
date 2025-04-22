525b7d9c0c0ccbb34eb21521f0c25a7153660f80
Haisheng Chen <hac048@ucsd.edu>
2025-04-21 21:56:07 -0700

    update

--- scripts/longbench.sh
+++ scripts/longbench.sh
@@ -3,12 +3,12 @@ cd evaluation/LongBench
 model="/mnt/R1-Distill-llama-8B"
 
 #for task in "qasper" "narrativeqa" "hotpotqa" "multifieldqa_en" "gov_report" "triviaqa"
-for task in "hotpotqa"  "multifieldqa_en" "gov_report"  "triviaqa"
+for task in "hotpotqa"  "multifieldqa_en" #"gov_report"  "triviaqa"
 do
    python -u pred.py \
         --model $model --task $task
 
-    for budget in 512 1024 2048 4096
+    for budget in 512 1024 2048
      do
         python -u pred.py \
             --model $model --task $task \
